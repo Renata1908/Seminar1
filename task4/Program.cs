6ac13495d8813fc0867d9b1b162156c9464f83d2
Renata19 <renatalsa@mail.ru>
2022-09-03 12:44:01 +0700

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+
+{
+int count = number % 10;
+Console.WriteLine(count);
+}
+else
+
+{
+    Console.WriteLine("Введено не трехзначное число!");
+}
+

5b292cdd1024b6656f441b567cd12cf58da10aa9
Renata19 <renatalsa@mail.ru>
2022-09-03 11:34:39 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("Да ");
+}
+else
+{
+        Console.WriteLine("Нет ");
+}
